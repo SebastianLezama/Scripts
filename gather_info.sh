84bc01b592a2caa9b9dd13b3a879d408cd176abc
Sebastian Lezama <sebastian.lezama@gmail.com>
2022-06-03 15:32:48 -0300

    Added if statement

--- gather_info.sh
+++ gather_info.sh
@@ -1,11 +1,20 @@
 #! /bin/bash
 
-echo "Starting at: $(date)"; echo
+line="---------------------------------------------------------"
+echo "Starting at: $(date)"; echo $line
 
-echo "UPTIME"; uptime; echo
+echo "UPTIME"; uptime; echo $line
 
-echo "FREE"; free ; echo
+echo "FREE"; free ; echo $line
 
-echo "WHO"; who ; echo
+echo "WHO"; who ; echo $line
+
+echo "HOST"
+if grep "127.0.0.1" /etc/hosts; then
+    echo "Everything OK"
+else
+    echo "ERROR! 127.0.0.1 is not in /etc/hosts"
+fi
+echo $line
 
 echo "Finishing at: $(date)"
